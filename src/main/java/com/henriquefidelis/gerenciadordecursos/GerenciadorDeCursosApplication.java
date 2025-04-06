d11af7c1af392addf1a5f5c3d4bb3f8cb9dc3ccb
jhfidelis <fidelis.henrique.jh@gmail.com>
2025-04-05 23:52:42 -0300

    Basic project structure

---
+++ src/main/java/com/henriquefidelis/gerenciadordecursos/GerenciadorDeCursosApplication.java
@@ -0,0 +1,13 @@
+package com.henriquefidelis.gerenciadordecursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeCursosApplication.class, args);
+	}
+
+}
