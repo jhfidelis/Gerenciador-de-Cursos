37424d9bf2754240c0f280fb5622b9e02cfc1915
jhfidelis <fidelis.henrique.jh@gmail.com>
2025-04-06 21:54:00 -0300

    Create the course table

--- src/main/java/com/henriquefidelis/gerenciadordecursos/modules/courses/entities/CourseEntity.java
+++ src/main/java/com/henriquefidelis/gerenciadordecursos/modules/courses/entities/CourseEntity.java
@@ -2,23 +2,39 @@ package com.henriquefidelis.gerenciadordecursos.modules.courses.entities;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_course")
 public class CourseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @NotBlank(message = "O nome do curso é um campo obrigatório")
+    @Column(nullable = false)
     private String name;
 
     @NotBlank(message = "A categoria do curso é um campo obrigatório")
+    @Column(nullable = false)
     private String category;
 
     private Boolean Active;
 
+    @CreationTimestamp
     private LocalDateTime createdAt;
+
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
     
 }
